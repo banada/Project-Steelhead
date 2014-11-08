ba93fd802bb9edbfd98b3714d0cad98eac02efbd
Nathaniel Chen <nathanielchen1@gmail.com>
2014-11-08 09:27:04 +0800

    Character movement, Protagonist controller script
    Basic character movement:
    -Walk
    -Jump
    -Dive
    -Slide
    Controller script for the main character enables this functionality.

---
+++ Scripts/Movement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+
+	//Check for objects in Ground layer
+	[SerializeField] LayerMask marked_ground;
+	Transform bottom;
+	Transform top;
+	Vector2 dive_vector;
+	//float to_ground;
+
+	void Awake() {
+		bottom = transform.Find("Bottom");
+		top = transform.Find ("Top");
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate() {
+	}
+
+	// Lateral Movement
+	public void move(float axis, float move_speed) {
+		rigidbody2D.velocity = new Vector2(axis*move_speed, rigidbody2D.velocity.y);
+	}
+
+	// Jumping
+	public void jump(float jump_force) {
+		if (is_grounded()) {
+			rigidbody2D.AddForce(new Vector2(0f, jump_force));
+		}
+	}
+
+	// Lateral slide
+	public void slide(float slide_force) {
+		rigidbody2D.AddForce(new Vector2(slide_force, 0f));
+	}
+
+	// Downward dive
+	public void dive(float dive_force_x, float dive_force_y) {
+		dive_vector = new Vector2(dive_force_x, dive_force_y);
+		rigidbody2D.MovePosition(rigidbody2D.position + dive_vector*Time.deltaTime);
+	}
+
+	// Checks if character is touching ground
+	public bool is_grounded(float gnd_chk_radius=0.1f) {
+		bool grounded = Physics2D.OverlapCircle(bottom.position, gnd_chk_radius, marked_ground);
+		//bool grounded = Physics2D.Raycast(bottom.position, -Vector2.up, 1f);
+		return grounded;
+	}
+
+	// Remove all velocity
+	public void freeze() {
+		rigidbody2D.velocity = new Vector2(0, 0);
+	}
+
+}
---
+++ Scripts/ProtagonistControl.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Movement))]
+
+public class ProtagonistControl : MonoBehaviour {
+
+	[SerializeField] float move_speed = 10f;
+	[SerializeField] float jump_force = 1000f;
+	[SerializeField] float slide_force = 30f;
+	[SerializeField] float dive_force_x = 30f;
+	[SerializeField] float dive_force_y = -40f;
+	float gnd_chk_radius = 0.1f;
+	
+	private Movement movement;
+	private bool jump;
+	// Lock player movement when diving, etc.
+	private bool is_locked = false;
+	private bool is_diving = false;
+
+	void Awake() {
+		movement = GetComponent<Movement>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Input.GetButtonDown("Jump")) && (!is_locked)) {
+			movement.jump(jump_force);
+		}
+		if (Input.GetButton("Ctrl")) {
+			if (!is_locked) {
+				if (movement.is_grounded(gnd_chk_radius)) {
+					movement.slide(slide_force);
+				} else {
+						movement.dive(dive_force_x, dive_force_y);
+						is_locked = true;
+						is_diving = true;
+				}
+			}
+		}
+		if (is_diving) {
+			if (movement.is_grounded(gnd_chk_radius)) {
+				// Rigidbody2D can bounce weirdly on grounding if character moving
+				movement.freeze();
+				is_locked = false;
+				is_diving = false;
+			} else {
+				movement.dive(dive_force_x, dive_force_y);
+			}
+		}
+	}
+	
+	//use FixedUpdate instead of Update on Rigidbody
+	void FixedUpdate() {
+		//gets change in position
+		float axis = Input.GetAxis("Horizontal");
+		//move player
+		if (!is_locked) {
+			movement.move(axis, move_speed);
+		}
+	}
+}
